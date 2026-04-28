a473e88643dee2e37be120148f2a5290a86f721e
brentfpage <brentfpage@gmail.com>
2026-04-28 16:43:40 -0700

    compilable and installable on rooted smt380

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,11 +4,16 @@
 OUT_DIR=out
 
 # you should change the "CROSS_COMPILE" to right toolchain path (you downloaded)
-# ex)CROSS_COMPILE={android platform directory you downloaded}/android/prebuilt/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=arm-linux-androideabi- KCFLAGS=-mno-android"
+# ex)
+# CROSS_COMPILE=~/android/prebuilt/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CROSS_COMPILE=/home/brentfpage_gmail_com/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=${CROSS_COMPILE} KCFLAGS=-mno-android"
 
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
+# export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
+# export PATH=$(pwd)/../arm-linux-androideabi-4.9/bin:$PATH
+export PATH=/home/brentfpage_gmail_com/arm-linux-androideabi-4.9/bin:$PATH
 export ARCH=arm
+alias python='python2'
 
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
 mkdir ${OUT_DIR}
