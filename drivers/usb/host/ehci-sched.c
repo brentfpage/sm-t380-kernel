adedf32cb4268fc1727f4a195f97c51db543830f
brentfpage <brentfpage@gmail.com>
2026-06-06 23:30:07 -0700

    deleted draft/crap code

--- drivers/usb/host/ehci-sched.c
+++ drivers/usb/host/ehci-sched.c
@@ -2205,33 +2205,6 @@ sitd_patch(
 static inline void
 sitd_link (struct ehci_hcd *ehci, unsigned frame, struct ehci_sitd *sitd)
 {
-    __hc32          type, *hw_p;
-
-	sitd->hw_next = EHCI_LIST_END(ehci);
-	sitd->frame = frame;
-
-    hw_p = &ehci->periodic [frame];
-    type = Q_NEXT_TYPE(ehci, *hw_p);
-
-    switch (hc32_to_cpu(ehci, type)) {
-        case Q_TYPE_ITD:
-            ehci->pshadow [frame].itd->itd_next = sitd;
-            ehci->pshadow [frame].itd->hw_next = sitd->sitd_dma;
-            break;
-        case Q_TYPE_SITD:
-            ehci->pshadow [frame].sitd->sitd_next = sitd;
-            ehci->pshadow [frame].sitd->hw_next = sitd->sitd_dma;
-            break;
-        case Q_TYPE_QH:
-        case Q_TYPE_FSTN:
-
-        default:
-            ehci_dbg(ehci, "sitd_link corrupt type %d frame %d shadow %pK\n",
-                type, frame, q.ptr);
-        default:
-
-    
-
 	/* note: sitd ordering could matter (CSPLIT then SSPLIT) */
 	sitd->sitd_next = ehci->pshadow [frame];
 	sitd->hw_next = ehci->periodic [frame];
@@ -2239,9 +2212,6 @@ sitd_link (struct ehci_hcd *ehci, unsigned frame, struct ehci_sitd *sitd)
 	sitd->frame = frame;
 	wmb ();
 	ehci->periodic[frame] = cpu_to_hc32(ehci, sitd->sitd_dma | Q_TYPE_SITD);
-
-// 	wmb ();
-// 	ehci->periodic[frame] = cpu_to_hc32(ehci, sitd->sitd_dma | Q_TYPE_SITD);
 }
 
 /* fit urb's sitds into the selected schedule slot; activate as needed */
