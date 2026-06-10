26708e3f764591e61bb239cb63549a40b75f6092
brentfpage <brentfpage@gmail.com>
2026-06-09 18:29:57 -0700

    fix typo

--- drivers/usb/host/ehci-sched.c
+++ drivers/usb/host/ehci-sched.c
@@ -2190,7 +2190,7 @@ sitd_patch(
         } else  {
             sitd->hw_uframe=stream->c_splits2;
         }
-        transaction |= cpu_to_hc32(SITD_STS_STS); /* start in Do Complete Split mode, ehci1 4.12.3.3.2.1*/
+        transaction |= cpu_to_hc32(ehci, SITD_STS_STS); /* start in Do Complete Split mode, ehci1 4.12.3.3.2.1*/
     }
 	sitd->hw_results = transaction;
 
