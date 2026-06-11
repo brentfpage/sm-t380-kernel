ff80affc84f98b1eaeaf173b58ed7d897641d456
brentfpage <brentfpage@gmail.com>
2026-06-11 08:32:52 -0700

    again try: start scanning further into the past relative to the previous scan endpoint

--- drivers/usb/host/ehci-sched.c
+++ drivers/usb/host/ehci-sched.c
@@ -2615,7 +2615,7 @@ restart:
 			break;
 
 		/* The last frame may still have active siTDs */
-		ehci->last_iso_frame = frame;
+		ehci->last_iso_frame = (frame - 1) & fmask;
 		frame = (frame + 1) & fmask;
 	}
 }
